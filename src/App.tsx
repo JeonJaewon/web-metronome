b607e8d9c38530cc33cf248aa55e4aadfcc155b1
Jaewon Jeon <wksk04515@naver.com>
2025-07-01 02:02:18 +0900

    Implement dynamic island feature with motion effects in App component

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,42 @@ import { Stopwatch } from "@/components/Stopwatch/Stopwatch";
 import { VisualizerSwitch } from "@/components/VisualizerSwitch/VisualizerSwitch";
 import { VolumeController } from "@/components/VolumeController/VolumeController";
 import { Flex } from "@mantine/core";
+import { motion } from "motion/react";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [isIsland, setIsIsland] = useState(false);
+
   return (
-    <div className={styles.wrapper}>
-      <VisualizerSwitch />
-      <BPMController />
-      <Flex
-        mt="xl"
-        p="lg"
-        gap="20px"
-        direction="column"
-        bd="1px solid #ddd"
-        bdrs={8}
-        justify="center"
-        align="center"
+    <>
+      <button onClick={() => setIsIsland((v) => !v)}>to dynamic island</button>
+      <motion.div
+        className={styles.wrapper}
+        initial={{ y: 0, scale: 1 }}
+        animate={isIsland ? { y: -300, scale: 0.8 } : { y: 0, scale: 1 }}
+        transition={{ type: "spring", bounce: 0.1, duration: 0.25 }}
       >
-        <BeatSelector />
-        <AccentToggleController />
-      </Flex>
-      <Flex gap="sm" mt="xl">
-        <PlayButton />
-        <VolumeController />
-      </Flex>
-      <Stopwatch />
-    </div>
+        {!isIsland && <VisualizerSwitch />}
+        <BPMController />
+        <Flex
+          mt="xl"
+          p="lg"
+          gap="20px"
+          direction="column"
+          bd="1px solid #ddd"
+          bdrs={8}
+          justify="center"
+          align="center"
+        >
+          <BeatSelector />
+          <AccentToggleController />
+        </Flex>
+        <Flex gap="sm" mt="xl">
+          <PlayButton />
+          <VolumeController />
+        </Flex>
+        <Stopwatch />
+      </motion.div>
+    </>
   );
 }
-
-export default App;
