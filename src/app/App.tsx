4ffc6c0ffd349ff92f3d7dfbc2f4493d8500275d
Jaewon Jeon <wksk04515@naver.com>
2025-07-02 21:26:10 +0900

    Add AppProvider and feature context for managing focused features; implement App component with motion effects and layout structure.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,4 @@
-import * as styles from "@/App.css";
+import * as styles from "@/app/App.css";
 import { AccentToggleController } from "@/components/AccentToggleController/AccentToggleController";
 import { BeatSelector } from "@/components/BeatSelector/BeatSelector";
 import { BPMController } from "@/components/BPMController/BPMController";
@@ -6,23 +6,29 @@ import { PlayButton } from "@/components/PlayButton/PlayButton";
 import { Stopwatch } from "@/components/Stopwatch/Stopwatch";
 import { VisualizerSwitch } from "@/components/VisualizerSwitch/VisualizerSwitch";
 import { VolumeController } from "@/components/VolumeController/VolumeController";
+import { useFeatureContext } from "@/contexts/featureContext";
 import { Flex } from "@mantine/core";
 import { motion } from "motion/react";
-import { useState } from "react";
 
 export default function App() {
-  const [isIsland, setIsIsland] = useState(false);
+  const { focusedFeature, setFocusedFeature } = useFeatureContext();
 
   return (
     <>
-      <button onClick={() => setIsIsland((v) => !v)}>to dynamic island</button>
+      <button onClick={() => setFocusedFeature("guitarScales")}>
+        Switch to Guitar Scales
+      </button>
       <motion.div
         className={styles.wrapper}
         initial={{ y: 0, scale: 1 }}
-        animate={isIsland ? { y: -300, scale: 0.8 } : { y: 0, scale: 1 }}
-        transition={{ type: "spring", bounce: 0.1, duration: 0.25 }}
+        animate={
+          focusedFeature !== "metronome"
+            ? { y: -300, scale: 0.8 }
+            : { y: 0, scale: 1 }
+        }
+        transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
       >
-        {!isIsland && <VisualizerSwitch />}
+        {focusedFeature === "metronome" && <VisualizerSwitch />}
         <BPMController />
         <Flex
           mt="xl"
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import App from "./app/App.tsx";
 import "./index.css";
+import { AppProvider } from "./app/AppProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MantineProvider>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </MantineProvider>
   </StrictMode>
 );
